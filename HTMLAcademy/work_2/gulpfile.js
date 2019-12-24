4a3dd13eaee313598ebd8964aa8da61d26510d92
pashkovichD <d.pashkovich@gmail.com>
2019-12-24 12:29:01 +0300

    work

---
+++ HTMLAcademy/work_2/gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var gulp = require("gulp");
+var less = require("gulp-less");
+
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+
+// gulp.task("less", function() {
+// 	gulp.src("less/style.less")
+// 		.pipe(less())
+// 		.pipe(gulp.dest("css"));
+// });
+
+gulp.task("style", function() {
+	gulp.src("less/**/*.less")
+		.pipe(plumber())
+		.pipe(less())
+		.pipe(postcss([
+			autoprefixer()
+		]))
+		.pipe(gulp.dest("css"))	
+		.pipe(server.stream());
+});
+
+gulp.task("server", ["style"], function() {
+	server.init({
+		server: ".",
+		notify: false,
+		open: true,
+		cors: true,
+		ui: false
+	});
+
+	gulp.watch("less/**/*.less", ["style"]);
+	gulp.watch("*.html").on("change", server.reload);
+});
