dbc522902b710cd07852298552835379ab0d0a8a
pashkovichD <d.pashkovich@gmail.com>
2020-01-23 12:48:00 +0300

    work

--- jreoby/source/js/common.js
+++ jreoby/source/js/common.js
@@ -4,6 +4,8 @@ var search = document.querySelector('.page-header__toggle-search');
 
 var nav = document.querySelector('.main-nav__list');
 var menuItem = document.querySelectorAll('.menu-list__item');
+var dropdown= document.querySelectorAll('.menu-list__dropdown');
+
 
 /* работа кнопки меню-гамбургера */
 hamburger.addEventListener('click', function (e) {
@@ -27,6 +29,39 @@ search.addEventListener('click', function (e) {
 menuItem.forEach(function(el) { // перебираем массив
 	el.addEventListener('click', function (e) {
 		// e.preventDefault();		
-		this.classList.toggle('menu-list__item--active');				
+		// hideDropdown();
+		parent = this;
+		
+		// parent.classList.toggle('menu-list__item--active');
+		// if(parent.classList.contains == 'menu-list__item--active') {
+		// 	alert('OK');
+		// }
+		dropdown.forEach(function(el) {
+			if(el.parentNode == parent) {
+				if(parent.classList.contains('menu-list__item--active')) {
+					hideDropdown();
+					// el.style.height = 0 + 'px';
+					parent.classList.remove('menu-list__item--active');
+				} else {
+					parent.classList.add('menu-list__item--active');
+					el.style.height = el.scrollHeight + 'px';	
+				}
+				
+
+				/*if(parent.classList.contains == 'menu-list__item--active') {
+					el.style.height = 0 + 'px';
+					parent.classList.remove('menu-list__item--active');
+				} else {
+					parent.classList.add('menu-list__item--active');
+					el.style.height = el.scrollHeight + 'px';
+				}*/
+			}			
+		});	
 	});
 });
+
+function hideDropdown() { //скрыть все подменю и убрать активный класс
+	dropdown.forEach(function(el) {
+		el.style.height = '0';			
+	});	
+}
