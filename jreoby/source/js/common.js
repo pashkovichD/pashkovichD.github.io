22d8ac548b510959ea61960b1e429c383142c2b8
pashkovichD <d.pashkovich@gmail.com>
2020-01-23 09:17:53 +0300

    work

--- jreoby/source/js/common.js
+++ jreoby/source/js/common.js
@@ -3,6 +3,7 @@ var search = document.querySelector('.page-header__toggle-search');
 
 
 var nav = document.querySelector('.main-nav__list');
+var menuItem = document.querySelectorAll('.menu-list__item');
 
 /* работа кнопки меню-гамбургера */
 hamburger.addEventListener('click', function (e) {
@@ -20,4 +21,12 @@ search.addEventListener('click', function (e) {
 	e.preventDefault();
 	// this.classList.toggle('hamburger__click');
 	alert("OK");
+});
+
+/* show поиска */
+menuItem.forEach(function(el) { // перебираем массив
+	el.addEventListener('click', function (e) {
+		// e.preventDefault();		
+		this.classList.toggle('menu-list__item--active');				
+	});
 });
