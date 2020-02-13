70c0ecaff667a3976e194244fe937fe59c66f1ea
pashkovichD <d.pashkovich@gmail.com>
2020-02-13 16:47:38 +0300

    work

--- jreoby/source/js/form.js
+++ jreoby/source/js/form.js
@@ -1,56 +1,3 @@
-// var titles = document.querySelectorAll('.accordeon__title');
-// var contents = document.querySelectorAll('.accordeon__content');
-
-
-// titles.forEach(function(el) { // перебираем массив
-// 	el.addEventListener('click', function (e) {		
-// 		var title = this;
-// 		contents.forEach(function(el) {
-// 			if(el.previousElementSibling == title) {
-// 				if(title.classList.contains('accordeon__title--active')) {
-// 					hideDropdown();
-// 				} else {
-// 					hideDropdown();
-// 					title.classList.add('accordeon__title--active');
-// 					el.style.height = el.scrollHeight + 'px';
-// 				}
-// 			}
-// 		});
-// 	});
-// });
-
-// function hideDropdown() { 
-// 	contents.forEach(function(el) { //скрыть все подменю
-// 		el.style.height = '0';			
-// 	});
-// 	titles.forEach(function(el) { //убрать активный класс
-// 		el.classList.remove('accordeon__title--active');
-// 	});
-// }
-
-// $('.question input:not([type=submit]), .question textarea').focus(function() {
-// 	if($(this).val() == '') {
-// 		$(this).next('label').animate({top: '9px', fontSize: '15px'}, 300, 'easeOutQuint').css({color: '#319A00'});
-// 		$(this).css({borderColor: '#319A00'});
-// 	}
-// });
-
-// $('.question input:not([type=submit]), .question textarea').blur(function() {
-// 	if($(this).val() == '') {
-// 		if(this.nodeName == 'TEXTAREA') {
-// 			$(this).next('label').animate({top: '30px', fontSize: '16px'}, 300, 'easeOutQuint').css({color: '#c3c3c3'});
-// 			$(this).css({borderColor: '#d0d0d0'});
-// 		} else {
-// 			$(this).next('label').animate({top: '50%', fontSize: '16px'}, 300, 'easeOutQuint').css({color: '#c3c3c3'});
-// 			$(this).css({borderColor: '#d0d0d0'});
-// 		}
-// 	} else {
-// 		$(this).next('label').css({color: '#e0e0e0'});
-// 		$(this).css({borderColor: '#e0e0e0'});
-// 	}
-// });
-
-// var inputs = document.querySelectorAll('.form__input');
 var inputs = document.querySelectorAll('.form__input');
 var labels = document.querySelectorAll('.form__label');
 
@@ -64,30 +11,29 @@ labels.forEach(function(el) {
 	el.classList.add('form__label--start'); // все label'ы в исходное состояние
 });
 
-inputs.forEach(function(el) {	
-	// el.addEventListener('mouseover', function (e) {
+inputs[0].focus(); // ставим фокус в первый input
+inputs[0].nextSibling.nextSibling.classList.remove('form__label--start'); // состояние фокуса в первом input'е
+
+inputs.forEach(function(el) {
 	el.addEventListener('focus', function (e) {
-	if(this.nextSibling.classList.contains('form__label--start')) {
-		// this.nextSibling.classList.remove('form__label--start');
-		alert('OK');
-	}
-		// if(this.value == '') {
-		// 	alert('OK');
-		// }
+		lab = this.nextSibling.nextSibling;
+		if(lab.classList.contains('form__label--start')) {
+			lab.classList.remove('form__label--start');
+			// lab.style.color = '#329a00';
+			lab.style.color = '#e28605';
+		} else {
+			// lab.style.color = '#329a00';
+			lab.style.color = '#e28605';
+		}
 	});
 
-	// el.onfocus = function() {		
-	// 	// if(this.nextSibling.classList.contains('form__label--start')) {
-	// 	// 	this.nextSibling.classList.remove('form__label--start')
-	// 	// }
-	// }
-	
-	// el.addEventListener('focus', function (e) {
-	// 	e.classList.add('red');
-	// });
+	el.addEventListener('blur', function (e) {
+		lab = this.nextSibling.nextSibling;
+		if(!lab.classList.contains('form__label--start')) {
+			lab.style.color = '#c3c3c3';
+			if(this.value == '') {
+				lab.classList.add('form__label--start');	
+			}
+		}
+	});
 });
-
-
-// inputs.addEventListener('focus', function (e) {
-// 	// e.classList.add('red');
-// });
