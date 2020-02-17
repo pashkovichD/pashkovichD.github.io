f87bde04225cc98e33d7efabd2bac31a532e2086
pashkovichD <d.pashkovich@gmail.com>
2020-02-17 11:37:09 +0300

    work

--- jreoby/source/js/form.js
+++ jreoby/source/js/form.js
@@ -10,7 +10,7 @@ var formNotice = document.querySelectorAll('.form__notice');
 var formError = document.querySelector('.form__notice--error');
 var formSuccesfull = document.querySelector('.form__notice--succesfull');
 
-
+var files_error = false;
 
 // удаляем стили для отображения формы, на случай, когда не работает JS
 var forms = document.querySelectorAll('.form'); // на странице таких форм может быть несколько
@@ -26,11 +26,10 @@ inputs[0].focus(); // ставим фокус в первый input
 inputs[0].nextSibling.nextSibling.classList.remove('form__label--start'); // состояние фокуса в первом input'е
 
 inputs.forEach(function(el) {
-	
-	alert(el.value);
 
+	/* если при запуске странице в поле есть значение value... например, при возврате на эту страницу после отправки формы */
 	if(el.value != '') {
-		lab = this.nextSibling.nextSibling;
+		lab = el.nextSibling.nextSibling;
 		lab.classList.remove('form__label--start');
 	}
 
@@ -38,10 +37,8 @@ inputs.forEach(function(el) {
 		lab = this.nextSibling.nextSibling;
 		if(lab.classList.contains('form__label--start')) {
 			lab.classList.remove('form__label--start');
-			// lab.style.color = '#329a00';
 			lab.style.color = '#e28605';
 		} else {
-			// lab.style.color = '#329a00';
 			lab.style.color = '#e28605';
 		}
 	});
@@ -61,17 +58,34 @@ inputs.forEach(function(el) {
 /*--- Примечание: Была проблема с работой нескольких input file... как потом оказалось нужно всем (если их несколько на странице) input file давать разные id и соответствующим label такие же for. Иначе всё работает только на первый input file ---*/
 attach.forEach(function(el) {
 	el.addEventListener('change', function (e) { // слушаем событие изменения input file
-		str = this.value; // полное имя выбранного файла
+		// str = this.value; // полное имя выбранного файла
+		files_error = true;
+		files = el.files; // массив прикрепленных файлов
 
+		size_allow = 2048; // Kb
+		ext_allow = ["doc", "docx", "rtf", "jpg", "jpeg", "bmp", "png", "pdf", "txt", "zip", "rar"]; // допустимые форматы
+
+		file_size = Math.round(files[0].size/1024); // в Kb
+		file_ext = files[0].name.split(".").splice(-1,1)[0].toLowerCase();
+
+		ext_allow.forEach(function(el) {
+			if(el == file_ext) {
+				if(file_size <= size_allow) {
+					files_error = false;
+				}
+				
+			}
+		});
 		// находим инденкс элемента, после которого начинается имя файла
-		if (str.lastIndexOf('\\')){
+		/*if (str.lastIndexOf('\\')){
 			var i = str.lastIndexOf('\\')+1;
 		}
 		else{
 			var i = str.lastIndexOf('/')+1;
 		}
-
-		var filename = str.slice(i); // получаем имя файла			
+		var filename = str.slice(i); // получаем имя файла*/
+		
+		var filename = el.files[0].name; // получаем имя файла
 		
 		parent = this.parentNode; // определяем родителя у загружаемого input[file], чтобы потом по общему родителю найти нужный параграф form__filename для записи в него имени файла
 
@@ -80,7 +94,6 @@ attach.forEach(function(el) {
 				t.innerHTML = filename; // записываем имя файла
 			}
 		});
-		
 	});
 });
 
@@ -102,7 +115,11 @@ form.addEventListener('submit', function (e) {
 		}
 	});
 
-	if(error) {		
+	if(files_error) {
+		error = true;
+	}
+
+	if(error) {
 		formError.classList.add('form__notice--show');
 	}	else {		
 		formSuccesfull.classList.add('form__notice--show');
@@ -115,78 +132,3 @@ function hideNotice() {
 		el.classList.remove('form__notice--show');
 	});
 }
-
-// var el_field = new Array("el_firstname", "el_name", "el_secondname", "el_address", "el_email", "el_message");//поля обязательные 
-
-// $("#el_form").submit(function() {// обрабатываем отправку формы
-
-// $(".el_error").fadeOut(); // скрываем поле
-// $(".el_error_file").fadeOut(); // скрываем поле
-
-// var error = 0;
-// var error_file = 0;    
-
-// // alert('ДО проверки error = ' + error);
-
-// $("#el_form").find(":input").each(function() {// проверяем каждое поле в форме
-
-// if($(this).attr("name") == "el_file") {       
-
-// if($(this).val()){// если в поле пусто              
-
-// error_file = 1;
-// var size_allow = 2000; // Kb
-// var ext_allow = ["doc", "docx", "rtf", "jpg", "jpeg", "bmp", "png", "pdf", "txt", "zip", "rar"]; // допустимые форматы
-
-// var file_name = document.getElementById('el_file').files[0].name;
-// var file_size = document.getElementById('el_file').files[0].size/1024; // в Kb
-// var file_ext = file_name.split(".").splice(-1,1)[0].toLowerCase();
-
-// for(var i = 0; i < ext_allow.length; i++) {
-// if(file_ext == ext_allow[i]) {
-// if(file_size <= size_allow) {
-// error_file = 0;
-// }
-// }
-// }
-// }
-// }
-
-// for(var i = 0; i < el_field.length; i++) { // если поле присутствует в списке обязательных        
-// if($(this).attr("name") == el_field[i]) { //проверяем поле формы на пустоту
-
-// if($(this).attr("name") != "el_email") {
-// if(!$(this).val()){// если в поле пусто
-// error = 1; // определяем индекс ошибки
-// }
-// } else {
-// var mailRegExp = /^([a-zA-Z0-9_-]+\.)*[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)*\.[a-zA-Z]{2,6}$/;
-// if(!mailRegExp.test($(this).val())) {
-// error = 1; // определяем индекс ошибки
-// }
-// } 
-// }
-// }
-
-// });
-
-// // alert('ПОСЛЕ проверки error = ' + error);
-// // alert('ПОСЛЕ проверки error_file = ' + error_file);
-
-// if(error == 0 && error_file == 0){ // если ошибок нет то отправляем данные
-// return true;
-// } else {
-// if(error == 1) {
-// var err_text = "Проверьте, пожалуйста, правильность заполения всех полей!";
-// $(".el_error").html(err_text); 
-// $(".el_error").fadeIn(); 
-// }
-// if(error_file == 1) {
-// var err_file_text = "Файл слишком большого размера или не того формата!";
-// $(".el_error_file").html(err_file_text); 
-// $(".el_error_file").fadeIn(); 
-// }
-
-// return false; //если в форме встретились ошибки, не позволяем отослать данные на сервер
-// }
-// });  
