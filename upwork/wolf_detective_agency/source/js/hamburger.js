e6c4d8da28bd3bcdb65599851d036113b4a5877c
pashkovichD <d.pashkovich@gmail.com>
2021-03-26 13:17:47 +0300

    work

--- upwork/wolf_detective_agency/source/js/hamburger.js
+++ upwork/wolf_detective_agency/source/js/hamburger.js
@@ -1,17 +1,17 @@
-var hamburgerFirst = document.querySelector('.page-header__toggle-nav-first');
-var hamburgerSecond = document.querySelector('.page-header__toggle-nav-second');
+var hamburgerFirst = document.querySelector('.page-header__toggle-nav');
+// var hamburgerSecond = document.querySelector('.page-header__toggle-nav-second');
 var mainNav = document.querySelector('.main-nav');
-var secondNav = document.querySelector('.second-nav');
+// var secondNav = document.querySelector('.second-nav');
 
 hamburgerFirst.addEventListener('click', function (e) {
 	e.preventDefault();
 
 	this.classList.toggle('hamburger__click');
 	if(this.classList.contains('hamburger__click')) {
-		if(secondNav.classList.contains('second-nav--show')) {
-			secondNav.classList.remove('second-nav--show');
-			hamburgerSecond.classList.remove('hamburger__click');
-		}
+		// if(secondNav.classList.contains('second-nav--show')) {
+		// 	secondNav.classList.remove('second-nav--show');
+		// 	hamburgerSecond.classList.remove('hamburger__click');
+		// }
 		mainNav.classList.add('main-nav--show');
 		// leftHamburger = true; // menu show
 	} else {
@@ -20,14 +20,14 @@ hamburgerFirst.addEventListener('click', function (e) {
 	}	
 });
 
-hamburgerSecond.addEventListener('click', function (e) {
-	e.preventDefault();
+// hamburgerSecond.addEventListener('click', function (e) {
+// 	e.preventDefault();
 
-	this.classList.toggle('hamburger__click');
-	if(this.classList.contains('hamburger__click')) {
-		secondNav.classList.add('second-nav--show');
-		// leftHamburger = true; // menu show
-	} else {
-		secondNav.classList.remove('second-nav--show');		
-	}	
-});
+// 	this.classList.toggle('hamburger__click');
+// 	if(this.classList.contains('hamburger__click')) {
+// 		secondNav.classList.add('second-nav--show');
+// 		// leftHamburger = true; // menu show
+// 	} else {
+// 		secondNav.classList.remove('second-nav--show');		
+// 	}	
+// });
