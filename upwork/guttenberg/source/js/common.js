72485d5c84b66d35df9f1267aac2fdb07c11503d
pashkovichD <d.pashkovich@gmail.com>
2021-05-27 16:51:12 +0300

    work

--- upwork/guttenberg/source/js/common.js
+++ upwork/guttenberg/source/js/common.js
@@ -1,14 +1,5 @@
-var scrollLine = document.querySelector('.banner__scroll-line');
-
-scrollLine.classList.add('show-line');
-
-
-// navToggle.addEventListener('click', function() {
-// 	if(navMain.classList.contains('main-nav--closed')) {
-// 		navMain.classList.remove('main-nav--closed');
-// 		navMain.classList.add('main-nav--opened');
-// 	} else {
-// 		navMain.classList.add('main-nav--closed');
-// 		navMain.classList.remove('main-nav--opened');
-// 	}
-// });
+$(document).ready(function() {
+	$('.header__toolbar-search-toggle').on('click', function() {
+		$('.header-page').toggleClass('header-page--light');
+	});
+});
