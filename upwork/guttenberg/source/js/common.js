d8acb366524728cf3ff11213ac6d85321b15eff8
pashkovichD <d.pashkovich@gmail.com>
2021-06-07 12:14:05 +0300

    work

--- upwork/guttenberg/source/js/common.js
+++ upwork/guttenberg/source/js/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 	$('.header__toolbar-search-toggle').on('click', function() {
-		$('.header-page').toggleClass('header-page--light');
+		if($('.header-page').hasClass("header-page--light")) {
+			$('.header-page').addClass('header-page--dark');
+			$('.header-page').removeClass('header-page--light');
+		} else {
+			$('.header-page').addClass('header-page--light');
+			$('.header-page').removeClass('header-page--dark');
+		}
 	});
+
+
+	// $('.page-header__toggle-nav').on('click', function() {
+	// 	if($('.header-page').hasClass("page-header__toggle-nav")) {
+	// 		$('.header-page').addClass('header-page--dark');
+	// 		$('.header-page').removeClass('header-page--light');
+	// 	} else {
+	// 		$('.header-page').addClass('header-page--light');
+	// 		$('.header-page').removeClass('header-page--dark');
+	// 	}
+	// });
 });
