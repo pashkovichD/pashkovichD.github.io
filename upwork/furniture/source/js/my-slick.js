432b42aed3e48e783ef46e6b54cb1851c18a76ba
pashkovichD <d.pashkovich@gmail.com>
2021-08-30 16:13:17 +0300

    work

---
+++ upwork/furniture/source/js/my-slick.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+	$('.clients__list').slick({
+		infinite: true,
+		arrows: false,
+		dots: true,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		// centerMode: true,
+		variableWidth: true		
+	});	
+});
