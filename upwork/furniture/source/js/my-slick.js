894581821bf457297f25558df7d4ce6f943ec4ed
pashkovichD <d.pashkovich@gmail.com>
2021-08-31 19:21:55 +0300

    work

--- upwork/furniture/source/js/my-slick.js
+++ upwork/furniture/source/js/my-slick.js
@@ -5,7 +5,38 @@ jQuery(document).ready(function($) {
 		dots: true,
 		slidesToShow: 2,
 		slidesToScroll: 2,
-		// centerMode: true,
 		variableWidth: true		
-	});	
+	});
+
+	$(window).resize(function(){	    
+	    var $windowWidth = $(window).width();
+	    if ($windowWidth <= 1200) {
+	        slickfy();
+	    }
+	});
+
+	slickfy();
+
+	function slickfy() {
+		$('.works__list').slick({
+			// autoplay: true,
+			infinite: true,
+			arrows: false,
+			dots: true,		
+			variableWidth: true,
+			responsive: [{
+				breakpoint: 99999,
+				settings: "unslick"
+			}, {
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			}]
+		});	
+	}
+
+	
+	
 });
