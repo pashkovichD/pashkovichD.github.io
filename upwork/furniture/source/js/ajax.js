11d2b3ed6b82d104965c2932802c69e004a123fc
pashkovichD <d.pashkovich@gmail.com>
2021-09-20 17:00:50 +0300

    work

---
+++ upwork/furniture/source/js/ajax.js
@@ -0,0 +1,23 @@
+function sendAjaxForm(result_form, ajax_form, url) {
+    $.ajax({
+        url:     url, //url (action_ajax_form.php)
+        type:     "POST",
+        dataType: "html",
+        data: $("#"+'contact-form').serialize(),
+        success: function(response) {
+            result = $.parseJSON(response);         
+            $(document).mouseup(function (e){ // click on web document
+                var div = $('.form__result'); // ID or class element
+                if (!div.is(e.target) // if the click was not on the element
+                    && div.has(e.target).length === 0) { // and not by its children
+                    div.removeClass('form__result--show');
+                }
+            });
+
+            $('.form__result').html('Successfully!').addClass('form__result--show');
+        },
+        error: function(response) {
+            $('.form__result').html('Error');
+        }
+    });
+}
--- upwork/furniture/source/js/form.js
+++ upwork/furniture/source/js/form.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function($) {	
 	
-	$('#contact-form').submit(function() {
-		// $('.form__result').text('').removeClass('form__result--show');
-		// alert(ok);
+	$('#contact-form').submit(function() {		
 		return false;
 	});
 
@@ -17,8 +15,14 @@ jQuery(document).ready(function($) {
 		}
 	})	
 	
+	// Mask Time
+	$.mask.definitions['f'] = "[0|1]";
+	$.mask.definitions['s'] = "[0|1|2|3|4|5]";
+	$.mask.definitions['p'] = "[a|p]";
+	$('#form__project_time').mask("f9:s9 pm" );
+
 	// DatePicker Jquery
-	$("#form__move_date").datepicker();
+	$("#form__project_date").datepicker();
 	$.datepicker.regional['en'] = {		
 		dateFormat: 'dd.mm.yy'
 	};
@@ -39,52 +43,16 @@ jQuery(document).ready(function($) {
 			form__name                : {required: true, minlength: 3},
 			form__phone               : {required: true},
 			form__email               : {required: true, email : true},			
-			form__move_time           : {required: false, time12h: true},
-			form__move_date           : {required: false, validDate: true}
+			form__project_time        : {required: false, time12h: true},
+			form__project_date        : {required: false, validDate: true}
 		},
 		messages: {
-			form__move_time : {time12h: 'Format <b>12-hour am/pm</b>'},
-			form__move_date : {validDate: 'Format <b>dd.mm.yyyy</b>.'},
+			form__project_time : {time12h: 'Format <b>12-hour am/pm</b>'},
+			form__project_date : {validDate: 'Format <b>dd.mm.yyyy</b>.'},
 		},
-		submitHandler: function (form) {
-	        // add a loading image in place of your returning outcome
-	        // $("#simple-msg").html("Sending...");
-	        // serialize/combine all submitted fields into an array
-	        var postData = $('#contact-form').serialize();
-	        // set url based of action
-	        var formURL = $('#contact-form').attr("action");	        
-	        $.ajax({
-	                type: "POST",
-	                url: formURL,
-	                data: postData,
-	                success:function(data, textStatus, jqXHR) {
-	                   $("#simple-msg").html('<p>Thanks for your request - we will be in touch soon!</p>');
-	                },
-	                error: function(jqXHR, textStatus, errorThrown) {
-	                   $("#simple-msg").html('<p>Message failed to send. Please try again!</p>');
-	                }
-	            });
-	        $('.form__result').text('Successfully!').addClass('form__result--show');
-
-			$(document).mouseup(function (e){ // click on web document
-				var div = $('.form__result'); // ID or class element
-				if (!div.is(e.target) // if the click was not on the element
-				    && div.has(e.target).length === 0) { // and not by its children
-					div.removeClass('form__result--show');
-				}
-			});
+		submitHandler: function(form) {
+			sendAjaxForm('form__result', 'contact-form', 'action_ajax_form.php');
 		}
-		/*submitHandler: function() {
-			$('.form__result').text('Successfully!').addClass('form__result--show');
-
-			$(document).mouseup(function (e){ // click on web document
-				var div = $('.form__result'); // ID or class element
-				if (!div.is(e.target) // if the click was not on the element
-				    && div.has(e.target).length === 0) { // and not by its children
-					div.removeClass('form__result--show');
-				}
-			});
-		}*/
 	});
 
 	
