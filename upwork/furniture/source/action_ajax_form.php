58b2a0a4b8ec0733917e37be8fa9feffd70c7620
pashkovichD <d.pashkovich@gmail.com>
2022-11-03 11:24:18 +0300

    work

--- upwork/furniture/source/action_ajax_form.php
+++ upwork/furniture/source/action_ajax_form.php
@@ -14,17 +14,12 @@
     	$receving = $_POST["receving"];
     	$certificate = $_POST["certificate"];
 
-    	$description = $_POST["form__description"];
-    	/*if (isset($_POST["form__description"])) {
-    		$description = $_POST["form__description"];
-    	} else {
-    		$description = ' ';
-    	}*/	  
+    	$description = $_POST["form__description"];    	
 
     	$message = "<div style='width: 550px; font-family: Arial, sans-serif; border-top: 1px dashed #aaaaaa;'><div style='display: flex; flex-wrap: nowrap; width: 100%; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Full Name</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $name . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Phone</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $phone . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Email</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $email . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Pickup Address</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $pickup_address . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Pickup Address Access</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $pickup_address_access . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Install Address</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $install_address . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Install Address Access</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $install_address_access . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Project Time</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $project_time . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Project Date</p>
 			<p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $project_date . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Receiving Needed</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $receving . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Certificate of Insurance Needed</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $certificate . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap; border-bottom: 1px dashed #aaaaaa;'><p style='width: 45%; margin: 0; padding: 10px 20px; font-weight: bold; background-color: #f2f2f2; text-align: right;'>Item List and Description</p><p style='width: 55%; margin: 0; padding: 10px 20px;'>" . $description . "</p></div><div style='display: flex; width: 100%; flex-wrap: nowrap;'><p style='width: 45%; margin: 0; padding: 0; font-weight: bold; text-align: right;'>&nbsp;</p><p style='width: 55%; margin: 0; padding: 0;'>&nbsp;</p></div></div>";
 
-	    send_mail($message, $name, $email);
+	    // send_mail($message, $name, $email);
 
 	    echo json_encode($message);
 	    
@@ -32,8 +27,8 @@
 	
     function send_mail($message, $name, $email){
         // e-mail to which the letter will come        
-        // $mail_to = "d.pashkovich@gmail.com";
-        $mail_to = "info@losangelesfurnitureinstallation.com";
+        $mail_to = "d.pashkovich@gmail.com";
+        // $mail_to = "info@losangelesfurnitureinstallation.com";
         // theme
         $subject = "Get a Quote (" . $name . ")";        
         // letter header
--- upwork/furniture/source/js/ajax.js
+++ upwork/furniture/source/js/ajax.js
@@ -5,7 +5,7 @@ function sendAjaxForm(result_form, ajax_form, url) {
         dataType: "html",
         data: $("#"+'contact-form').serialize(),
         success: function(response) {
-            result = $.parseJSON(response);         
+            /*result = $.parseJSON(response);         
             $(document).mouseup(function (e){ // click on web document
                 var div = $('.form__result'); // ID or class element
                 if (!div.is(e.target) // if the click was not on the element
@@ -14,7 +14,8 @@ function sendAjaxForm(result_form, ajax_form, url) {
                 }
             });
 
-            $('.form__result').html('Successfully!').addClass('form__result--show');
+            $('.form__result').html('Successfully!').addClass('form__result--show');*/
+            $('.form__result').html(response).addClass('form__result--show');
         },
         error: function(response) {
             $('.form__result').html('Error');
