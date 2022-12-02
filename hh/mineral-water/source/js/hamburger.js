834ebd0876a1db1dc21e99af0de230e950a28981
pashkovichD <d.pashkovich@gmail.com>
2022-12-02 16:56:24 +0300

    work

--- hh/mineral-water/source/js/hamburger.js
+++ hh/mineral-water/source/js/hamburger.js
@@ -1,13 +1,20 @@
 var hamburger = document.querySelector('.page-header__toggle-nav');
 var nav_wrapper = document.querySelector('.page-header__nav-wrapper');
+var nav = document.querySelector('.page-header__nav');
 
 hamburger.addEventListener('click', function (e) {
 	e.preventDefault();
 
 	this.classList.toggle('hamburger__click');
 	if(this.classList.contains('hamburger__click')) {
-		nav_wrapper.classList.add('page-header__nav--show');		
+		nav_wrapper.classList.add('page-header__nav-wrapper--show');
 	} else {
-		nav_wrapper.classList.remove('page-header__nav--show');
-	}	
+		nav_wrapper.classList.remove('page-header__nav-wrapper--show');
+	}
+
+	if(this.classList.contains('hamburger__click')) {
+		nav.classList.add('page-header__nav--show');
+	} else {
+		nav.classList.remove('page-header__nav--show');
+	}
 });
