c218d14187c89b4a1a2a6a4c46c6c21940377174
pashkovichD <d.pashkovich@gmail.com>
2023-11-24 00:56:48 +0300

    work

--- hh/aroma/source/js/swiper.js
+++ hh/aroma/source/js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.projects__list', {
   // Optional parameters
   direction: 'horizontal',
   loop: false,
@@ -10,9 +10,37 @@ const swiper = new Swiper('.swiper', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.projects__pagination .next',
+    prevEl: '.projects__pagination .prev',
   },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
+const swiperImg = new Swiper('.projects__item-images', {
+  // Optional parameters  
+  loop: true,
+  effect: 'fade',
+
+  // If we need pagination
+  // pagination: {
+  //   el: '.termi',
+
+  // },
+
+  pagination: {
+    el: '.projects__images-dots',
+    clickable: true,
+  },
+
+  //Navigation arrows
+  // navigation: {
+  //   nextEl: '.img-next',
+  //   prevEl: '.img-next',
+  // },
 
   // And if we need scrollbar
   // scrollbar: {
