66546271634d77ab7e43db10fab8f8ecc2836da6
pashkovichD <d.pashkovich@gmail.com>
2023-11-24 16:52:12 +0300

    work

---
+++ hh/aroma/source/js/popup.js
@@ -0,0 +1,38 @@
+var popup = document.querySelector('.modal-order');
+var iconCloseModal = document.querySelector('.modal-order__close');
+var overlay = document.querySelector('.overlay');
+var link = document.querySelector('.page-header__order');
+
+/* показ модального окна и overlay */
+link.addEventListener('click', function(evt) {	
+	evt.preventDefault();
+	popup.classList.remove('modal-hide');
+	popup.classList.add('modal-show');
+	overlay.classList.add('overlay-show');
+});
+
+
+iconCloseModal.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	hideModal();
+});
+
+overlay.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	hideModal();
+});
+
+/* скрытие модального окна */
+function hideModal() {
+	popup.classList.remove('modal-show');
+	popup.classList.add('modal-hide');	
+	overlay.classList.remove('overlay-show');
+}
+
+// open modal windows if click ESC
+window.addEventListener('keydown', function(evt) {
+	if(evt.keyCode === 27) {				
+		evt.preventDefault();		
+		hideModal();
+	}
+});
