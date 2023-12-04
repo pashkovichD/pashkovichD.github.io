5a4a21c4d8fd265868e6abd336d9d53890c3f563
pashkovichD <d.pashkovich@gmail.com>
2023-12-04 16:46:41 +0300

    work

--- hh/aroma/source/js/common.js
+++ hh/aroma/source/js/common.js
@@ -1,3 +1,41 @@
+var menuItem = document.querySelectorAll('.menu__item');
+var dropdown = document.querySelectorAll('.menu__dropdown');
+
+/* действия при изменении ширины экрана */
+window.onresize = function(event) {
+	var widthBody = document.body.clientWidth;
+};
+
+/* работа главного меню */
+menuItem.forEach(function(el) { // перебираем массив
+	el.addEventListener('click', function (e) {
+		// e.preventDefault(); // 
+		var parent = this;
+		dropdown.forEach(function(el) {
+			if(el.parentNode == parent) {
+				if(parent.classList.contains('menu__item--active')) {
+					hideDropdown();
+				} else {
+					hideDropdown();
+					parent.classList.add('menu__item--active');
+					el.style.height = el.scrollHeight + 'px';
+					// el.style.height = 'auto';
+				}				
+			}			
+		});	
+	});
+});
+
+function hideDropdown() { 
+	dropdown.forEach(function(el) { //скрыть все подменю
+		el.style.height = '0';			
+	});
+	menuItem.forEach(function(el) { //убрать активный класс
+		el.classList.remove('menu__item--active');
+	});
+}
+
+
 /* замена произвольного номера в href на нужный для телефона 'tel:+.............' */
 var number = document.querySelectorAll('.number-phone');
 
