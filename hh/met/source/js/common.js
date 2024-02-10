39d3ae65d0845c0740ec86908e8a540df7f4726c
pashkovichD <d.pashkovich@gmail.com>
2024-02-10 23:11:44 +0300

    work

--- hh/met/source/js/common.js
+++ hh/met/source/js/common.js
@@ -37,10 +37,10 @@ function hideDropdown() {
 
 
 /* замена произвольного номера в href на нужный для телефона 'tel:+.............' */
-var number = document.querySelectorAll('.number-phone');
+/*var number = document.querySelectorAll('.number-phone');
 
 number.forEach((num) => {	
 	href = num.getAttribute('href'); // получаем href
 	new_href = href.replace(/[^0-9]/g, ''); // удаляем все, кроме цифр	
 	num.setAttribute('href', 'tel:+' + new_href); // меняем значение href
-});
+});*/
--- hh/met/source/js/hamburger.js
+++ hh/met/source/js/hamburger.js
@@ -1,15 +1,15 @@
 var hamburger = document.querySelector('.page-header__toggle-nav');
 // var hamburgerList = document.querySelector('.page-header__toggle-list');
-// var nav = document.querySelector('.page-header__nav');
+var data = document.querySelector('.page-header__data-container');
 
 hamburger.addEventListener('click', function (e) {
 	e.preventDefault();
-
+	
 	this.classList.toggle('hamburger__click');
 	if(this.classList.contains('hamburger__click')) {
-		// nav.classList.add('page-header__nav--show');		
+		data.classList.add('page-header__data-container--show');
 	} else {
-		// nav.classList.remove('page-header__nav--show');
+		data.classList.remove('page-header__data-container--show');
 	}	
 });
 
