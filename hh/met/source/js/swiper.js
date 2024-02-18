4c0164be1f5e00f317854bcfc2124b306253382c
pashkovichD <d.pashkovich@gmail.com>
2024-02-19 01:27:58 +0300

    work

--- hh/met/source/js/swiper.js
+++ hh/met/source/js/swiper.js
@@ -111,3 +111,49 @@ const swiperProduction = new Swiper('.production__list', {
     }
   }
 });
+
+
+const swiperInfo = new Swiper('.info__list', {
+  // Optional parameters
+  slidesPerView: 1,
+  spaceBetween: 16,
+  width: 304, // ширина смещения слайдера
+
+  direction: 'horizontal',
+  loop: false,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.info__pagination .next',
+    prevEl: '.info__pagination .prev',
+  },
+
+  breakpoints: {    
+    // when window width is >= 768px
+    768: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+        width: 632, // ширина смещения слайдера
+    },
+    960: {       
+        slidesPerView: 2,
+        spaceBetween: 24,
+        width: 724, // ширина смещения слайдера
+    },
+    1200: {       
+        slidesPerView: 2,
+        spaceBetween: 30,
+        width: 866, // ширина смещения слайдера
+    },
+    1400: {       
+        slidesPerView: 2,
+        spaceBetween: 30,
+        width: 866, // ширина смещения слайдера
+    },
+    1600: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        width: 1314, // ширина смещения слайдера
+    }
+  }
+});
