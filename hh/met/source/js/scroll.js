8672824eb7aae058f9574a3c33c32bfbfa4d1e31
pashkovichD <d.pashkovich@gmail.com>
2024-02-23 20:46:09 +0300

    work

--- hh/met/source/js/scroll.js
+++ hh/met/source/js/scroll.js
@@ -1,53 +1,88 @@
 window.addEventListener('load', function(){
 	let menu = document.querySelector('.page-header__number');
+	let links = this.document.querySelectorAll('.page-header__number .page-header__number-item');
+	let header = document.querySelector('.page-header');
+	let classItem = 'page-header__number-item';
 	let classActiveItem = 'page-header__number-item--active';
-
-	/* Данная проверка нужна при переходе на страницу по ссылке и якорю, т.е. типа http://ссылка#якорь
-		В таком случае при переходе на страницу, страница открывается и плавно прокручивается к нужному блоку	
-	*/	
+	
 	if(window.location.hash != '') {
 		scrollToId(window.location.hash);
 	}
 	
 	menu.addEventListener('click', function(e){
-		// console.log('OK');
-		if(e.target.classList.contains('page-header__number-item')) { // если кликнули по ссылке из .page-header__number --- обрабатываем клик по методу ВСПЛЫТИЯ			
-			e.preventDefault(); // отменяем действие по умолчанию, т.к. в таком случаем всё равно резкий переход и behavior: 'smooth' (см. ниже) не срабатывает
-
-			
-
-			let link = e.target;
-			console.dir(link);
-			// console.log(link);
-			// console.dir(link); // список всех свойств выбранного объекта (среди них есть свойство hash, в котором и будет наш якорь)
-			scrollToId(link.hash);
-
-			/* Подсветка пункта меню. Легко сделать при клике (при переходе н страницу по ссылке типа http://ссылка#якорь нужно делать по-другому). */
-			/*menu.querySelector('.' + classActiveItem).classList.remove(classActiveItem);
-			link.classList.add(classActiveItem);*/
-
-
-
-			menu.querySelector('.page-header__number-item--active').classList.remove('page-header__number-item--active');
-			link.classList.add('page-header__number-item--active');
-		}
 		
+		if(e.target.classList.contains(classItem)) {
+			e.preventDefault();
+			let link = e.target;			
+			scrollToId(link.hash);			
+		}		
 	});
+
+	/*let btnUp = document.querySelector('.btnUp');
+	btnUp.addEventListener('click', function(e) {
+		scrollToY(0);
+	});*/
+
+	let scrolls = 0;
+	let scrollTimeout;
+	let delay = 100;
+	
+	window.addEventListener('scroll', function(e) {
+		clearTimeout(scrollTimeout); // ...удаляем setTimeout(), определенный ниже.
+		scrollTimeout = setTimeout(function() { // ставим выполнение задачи через delay (в нашем случае 100 мс). Если перестали scroll'ить, то через 100 мс сработает целевая функция onScroll(). Если же до 100 мс мы ещё раз про'scroll'или, то выше... и получаем новый интервал в 100 мс, если на протяжении этого интервала опять scroll'им, то получаем опять новый интервал на 10 мс и т.д. пока не "затихнем" на более 100 мс --- в таком случае сработает целевая функция.
+
+		// При таком подходе получаем минимум сработок целевой функции (получается, что целевая функция сработает только при завершении scroll'инга, после интервала в 100 мс). А, соответственно, увеличение производительности и ускорение работы кода.
+			
+			onScroll(e);
+		}, delay);
+	});
+
+	function onScroll(e) {		
+		console.log(++scrolls);
+		let pos = window.pageYOffset; // количество прокрученных (про'scroll'еных) пикселей		
+
+		/*if(pos > this.window.innerHeight) {
+			btnUp.classList.add('btnUp-open');
+		} else {
+			btnUp.classList.remove('btnUp-open');
+		}*/
+		
+		for(let i = links.length - 1; i >= 0; i--) {
+			let link = links[i];
+			let header = document.querySelector(link.hash);			
+
+			if(pos > (elemOffsetTop(header) - this.window.innerHeight/2)) {
+				menu.querySelector('.' + classActiveItem).classList.remove(classActiveItem);
+				link.classList.add(classActiveItem);
+				break;
+			}
+		}		
+	}
+
+	function scrollToId(id) {
+		let target = document.querySelector(id);
+		let styles = window.getComputedStyle(target); // стили целевого блока
+		let marginTopTarget = parseFloat(styles.marginTop); // верхний отступ целевого блока
+
+		let stylesHeader = window.getComputedStyle(header);	// стили header'а
+		let marginBottomHeader = parseFloat(stylesHeader.marginBottom); // нижний отступ header'а
+
+		if(target !== null) {			
+			let pos = elemOffsetTop(target) - header.clientHeight - marginBottomHeader - marginTopTarget; // вычитаем высоту header'а, нижний отступ header'а и верхний внешний отступ заголовка			
+			scrollToY(pos);
+		}
+	}
+
 });
 
-function scrollToId(id) {	
-	let target = document.querySelector(id);
+function scrollToY(pos) {
+	window.scrollTo({
+		top: pos,
+		behavior: 'smooth'
+	});
+}
 
-	/* ПРОВЕРКА. Пункт меню есть, а заголовка (id, который равен якорю ссылки) - нет. 
-		В таком случае будем получать ошибку в консоли. Т.е. в переменной target (см. выше) будет null (ничего не найдено)			
-		Правильнее, конечно, делать с помощью try {} catch... Но это уже позже
-	*/
-	if(target !== null) { // проверка на отсутствующий id (чтобы этого избежать). В таком случае код будет выполняться только при существующим id блока.
-		// console.log(target.offsetTop);
-		let pos = target.offsetTop - 70;
-		window.scrollTo({
-			top: pos,
-			behavior: 'smooth'
-		});
-	}
+function elemOffsetTop(node) {
+	let coords = node.getBoundingClientRect();
+	return window.pageYOffset + coords.top;
 }
