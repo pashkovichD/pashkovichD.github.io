2f35693c9f688ad0901e1523fda82e1482fc3019
pashkovichD <d.pashkovich@gmail.com>
2024-02-25 22:50:33 +0300

    work

--- hh/met/source/js/popup.js
+++ hh/met/source/js/popup.js
@@ -1,38 +1,47 @@
-var popup = document.querySelector('.modal-user');
-var iconCloseModal = document.querySelector('.modal-user__close');
-var overlay = document.querySelector('.overlay');
-var link = document.querySelector('.page-header__user');
+let modalUser = document.querySelector('.modal-user');
+let btnModalUser = document.querySelector('.page-header__user');
+let closeUser = document.querySelector('.modal-user__close');
 
-/* показ модального окна и overlay */
-link.addEventListener('click', function(evt) {	
-	evt.preventDefault();
-	popup.classList.remove('modal-hide');
-	popup.classList.add('modal-show');
-	overlay.classList.add('overlay-show');
-});
+let modalVideo = document.querySelector('.modal-video');
+let btnModalVideo = document.querySelector('.intro__video');
+let closeVideo = document.querySelector('.modal-video__close');
 
+let overlay = document.querySelector('.overlay');
 
-iconCloseModal.addEventListener('click', function(evt) {
-	evt.preventDefault();
-	hideModal();
-});
+workPopup(modalUser, btnModalUser, closeUser, overlay);
+workPopup(modalVideo, btnModalVideo, closeVideo, overlay);
 
-overlay.addEventListener('click', function(evt) {
-	evt.preventDefault();
-	hideModal();
-});
+function workPopup(popup, link, close, overlay) {
+	/* показ модального окна и overlay */
+	link.addEventListener('click', function(evt) {	
+		evt.preventDefault();
+		popup.classList.remove('modal-hide');
+		popup.classList.add('modal-show');
+		overlay.classList.add('overlay-show');
+	});
+
+	close.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		hideModal(popup, overlay);
+	});
+
+	overlay.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		hideModal(popup, overlay);
+	});
+
+	// open modal windows if click ESC
+	window.addEventListener('keydown', function(evt) {
+		if(evt.keyCode === 27) {
+			evt.preventDefault();
+			hideModal(popup, overlay);
+		}
+	});
+}
 
 /* скрытие модального окна */
-function hideModal() {
+function hideModal(popup, overlay) {
 	popup.classList.remove('modal-show');
 	popup.classList.add('modal-hide');	
 	overlay.classList.remove('overlay-show');
 }
-
-// open modal windows if click ESC
-window.addEventListener('keydown', function(evt) {
-	if(evt.keyCode === 27) {				
-		evt.preventDefault();		
-		hideModal();
-	}
-});
