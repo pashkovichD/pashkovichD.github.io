c987b04c582bb3394f2f92d188b93189b6d5427b
pashkovichD <d.pashkovich@gmail.com>
2024-05-20 09:47:23 +0300

    work

--- hh/vtop3/source/js/common.js
+++ hh/vtop3/source/js/common.js
@@ -40,14 +40,19 @@
 window.addEventListener('load', function() {
 	let lang = document.querySelector('.page-header__lang');	
 	let langText = lang.querySelector('span');
+	let clicked = false;
 	lang.addEventListener('click', function() {
-		langText.innerHTML = (langText.innerHTML === 'EN') ? 'RU' : 'EN';
-		langText.classList.add('lang__show');
-		setTimeout(removeClass, 2000);
+		if(!clicked) {
+			clicked = true;
+			langText.innerHTML = (langText.innerHTML === 'EN') ? 'RU' : 'EN';
+			langText.classList.add('lang__show');
+			setTimeout(removeClass, 2000);
+		}		
 	});
 
 	function removeClass() {
 		langText.classList.remove('lang__show');
+		clicked = false;
 	}	
 });
 
