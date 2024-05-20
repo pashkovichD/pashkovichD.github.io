0327cc907eaa85ca0aaf0306b6e4efd2b98266fe
pashkovichD <d.pashkovich@gmail.com>
2024-05-20 11:50:40 +0300

    work

--- hh/vtop3/source/js/hamburger.js
+++ hh/vtop3/source/js/hamburger.js
@@ -1,15 +1,15 @@
 var hamburger = document.querySelector('.page-header__toggle-nav');
 // var hamburgerList = document.querySelector('.page-header__toggle-list');
-var data = document.querySelector('.page-header__data-container');
+var nav = document.querySelector('.page-header__nav-container');
 
 hamburger.addEventListener('click', function (e) {
 	e.preventDefault();
 	
 	this.classList.toggle('hamburger__click');
 	if(this.classList.contains('hamburger__click')) {
-		data.classList.add('page-header__data-container--show');
+		nav.classList.add('page-header__nav-container--show');
 	} else {
-		data.classList.remove('page-header__data-container--show');
+		nav.classList.remove('page-header__nav-container--show');
 	}	
 });
 
