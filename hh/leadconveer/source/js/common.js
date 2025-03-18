1a20040e5750e9743c5b2e3e62fd41741c0026f3
pashkovichD <d.pashkovich@gmail.com>
2025-03-18 11:49:54 +0300

    work

--- hh/leadconveer/source/js/common.js
+++ hh/leadconveer/source/js/common.js
@@ -8,3 +8,24 @@ priceItem.forEach((item) => {
 		item.classList.add('price__item--active');
 	});	
 });
+
+
+let btnMore = document.querySelectorAll('.more__button');
+
+btnMore.forEach((btn) => {
+	btn.addEventListener('click', function(evt) {
+		evt.preventDefault();		
+		let more = this.nextElementSibling; // скрытый блок
+		let hide = more.querySelector('.more__hide'); // снопка "Скрыть"
+
+		more.style.maxHeight = more.scrollHeight + "px"; // свойство height делает равным его фактической высоте
+		btn.style.display = 'none';
+		hide.addEventListener('click', function() {
+			more.style.maxHeight = null; // делаем высоту равной 0
+			btn.style.display = 'block';
+		});
+	});	
+});
+
+
+
